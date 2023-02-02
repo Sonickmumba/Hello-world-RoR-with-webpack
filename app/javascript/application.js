9b8a83e81ab3df0a079b3dcdf13d9d3025d87718
sonickmumba <kingellie.mumba@gmail.com>
2023-02-02 09:25:52 +0200

    Add api routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (<h1>Hello World! Sonick</h1>);
+  return (<h1>Hello World! Sonick mm</h1>);
 }
 
 ReactDOM.render(
