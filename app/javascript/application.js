43267234d716bad8530eee319b66cc691907859f
sonickmumba <kingellie.mumba@gmail.com>
2023-02-02 22:45:07 +0200

    Create a store, an action,reducer and display random greetings

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,41 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './components/Greetings';
+import store from './redux/store';
+
+// function App() {
+//   return (
+//   <h1>
+//     Hello World! Sonick
+//   </h1>);
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root'),
+// );
+
 
 function App() {
-  return (<h1>Hello World! Sonick mm</h1>);
+  return (
+    <div>
+      <div>
+        <Routes>
+          <Route path = "/" element = { <Greeting /> } />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root'),
 );
